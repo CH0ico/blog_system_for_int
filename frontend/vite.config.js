889e76fffe4fecf5dc29ee372899737f8e9df63d
CH0ico <2741116499@qq.com>
2025-12-11 22:48:22 +0800

    home

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -28,7 +28,7 @@ export default defineConfig({
     host: "0.0.0.0",
     proxy: {
       "/api": {
-        target: "http://172.25.193.254:5000",
+        target: "http://localhost:5000",
         changeOrigin: true,
         secure: false,
       },
